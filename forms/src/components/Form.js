7ac617655ad4b97a4437e1b6fd5d69ccfd5e3ea7
Evghenia Raileanu <cantomd@mail.ru>
2021-07-16 23:30:38 -0700

    added validations

--- forms/src/components/Form.js
+++ forms/src/components/Form.js
@@ -6,6 +6,7 @@ const Form = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState( false );
 
     const createUser = (e) => {
         e.preventDefault();
@@ -16,11 +17,22 @@ const Form = (props) => {
         setEmail("");
         setPassword("");
         setConfirmPassword("");
+        setHasBeenSubmitted( true );
     };
     return(
         <div className="container pt-4 bg-dark text-white">
             <form onSubmit = { (e) => createUser (e)}>
+                {
+                    hasBeenSubmitted ?
+                    <h3 style={{ color: "red"}}>Thank you for submitting the form</h3> :
+                    <h3 style={{ color: "red"}} >Welcome, please submit the form:</h3>
+                }
                 <div>
+                    {
+                        firstName.length > 0 && firstName.length < 2 ?
+                        <p style={{color: "red"}}>First Name must be at least 2 characters</p> 
+                        : null
+                    }
                     <label style={{ width: "100px"}} for="firstName">First Name</label>
                     <input style={{ width: "300px", margin:"10px", padding: "5px"}} type="text" 
                     name = "firstName" 
@@ -28,6 +40,11 @@ const Form = (props) => {
                     onChange = { (e) => setFirstName(e.target.value) }/>
                 </div>
                 <div>
+                    {
+                        lastName.length > 0 && lastName.length < 2 ?
+                        <p style={{color: "red"}}>Last Name must be at least 2 characters</p> 
+                        : null
+                    }
                     <label style={{ width: "100px"}} for="lastName">Last Name</label>
                     <input style={{ width: "300px", margin:"15px", padding: "5px"}} type="text" 
                     name = "lastName" 
@@ -35,6 +52,11 @@ const Form = (props) => {
                     onChange = { (e) => setLastName(e.target.value) }/>
                 </div>
                 <div>
+                    {
+                        email.length > 0 && email.length < 2 ?
+                        <p style={{color: "red"}}>Email must be at least 2 characters</p> 
+                        : null
+                    }
                     <label style={{ width: "100px"}} for="email">Email</label>
                     <input  style={{ width: "300px", margin:"15px", padding: "5px"}} type="email" 
                     name = "email" 
@@ -42,6 +64,11 @@ const Form = (props) => {
                     onChange = { (e) => setEmail(e.target.value) }/>
                 </div>
                 <div>
+                    {   
+                        password.length > 0 && password.length < 8 ?
+                        <p style={{color: "red"}}>Password must be at least 8 characters</p> 
+                        : null
+                    }
                     <label style={{ width: "100px"}} for="password">Password</label>
                     <input style={{ width: "300px", margin:"15px", padding: "5px"}} type="password" 
                     name = "password" 
@@ -49,6 +76,13 @@ const Form = (props) => {
                     onChange = { (e) => setPassword(e.target.value) }/>
                 </div>
                 <div>
+                    {
+                        password.length >= 8 && confirmPassword.length > 0 ?
+                        password !== confirmPassword ?
+                        <p style={{color: "red"}}>Passwords do not match</p>
+                        : null
+                        : null
+                    }    
                     <label style={{ width: "100px"}} for="confirmPassword">Confirm Password</label>
                     <input style={{ width: "300px", margin:"2px 5px", padding: "5px"}} type="password" 
                     name = "confirmPassword" 
