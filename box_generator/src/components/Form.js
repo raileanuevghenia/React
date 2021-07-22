ecf1ca89d4aa8b3c2fe74c969694b63125b47c1d
Evghenia Raileanu <cantomd@mail.ru>
2021-07-22 00:28:45 -0700

    complete

--- box_generator/src/components/Form.js
+++ box_generator/src/components/Form.js
@@ -2,30 +2,46 @@ import React, { useState } from 'react';
 
 const Form = (props) => {
     const [newColor, setNewColor] = useState("");
+    const [size, setSize] = useState(200);
     const [boxesList, setBoxesList] = useState([]);
 
     const createBoxes = (e) => {
         e.preventDefault();
         setNewColor("");
-        setBoxesList( [...boxesList, newColor] );
+        setSize("");
+        setBoxesList( [...boxesList, {
+            color: newColor,
+            size: size + "px",
+        }
+        ] );
+
     }
     return(
         <div className="container pt-5">
             <form onSubmit = { (e) => createBoxes (e) }>
-                <label style = {{margin: "15px ", fontSize: "30px", fontWeight: "bold"}}>Color</label>
-                <input style = {{ borderRadius: "10px", width: "400px", height: "50px", backgroundColor: "silver"}}type="text"
-                name = "newColor"
-                value = { newColor }
-                onChange = { (e) => setNewColor(e.target.value) }/>
+                <div>
+                    <label style = {{margin: "8px ", fontSize: "30px", fontWeight: "bold"}}>Color</label>
+                    <input style = {{ borderRadius: "10px", width: "400px", height: "50px", backgroundColor: "silver"}}type="text"
+                    name = "newColor"
+                    value = { newColor }
+                    onChange = { (e) => setNewColor(e.target.value) }/>
+                </div>
+                <div>
+                    <label style = {{margin: "15px ", fontSize: "30px", fontWeight: "bold"}}>Size</label>
+                    <input style = {{ borderRadius: "10px", width: "400px", height: "50px", backgroundColor: "silver"}}type="text"
+                    name = "size"
+                    value = { size }
+                    onChange = { (e) => setSize(e.target.value) }/>
+                </div>
                 <button style = {{margin: "8px", borderRadius: "10px", width: "100px", height: "50px", fontSize: "20px"}}>Add</button>
             </form>
             {
-                boxesList.map((color, index) => (
+                boxesList.map((box, index) => (
                     <div key={ index }
                     style={{
-                    backgroundColor: color,
-                    width: "200px",
-                    height: "200px",
+                    backgroundColor: box.color,
+                    width: box.size,
+                    height: box.size,
                     margin: "20px",
                     display: "inline-block"
                     }}>
